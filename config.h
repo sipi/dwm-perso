16a03c9584b234063dd4dca0be81659d5d1913c4
Clément SIPIETER <clement.sipieter@lirmm.fr>
2014-07-08 22:21:28 +0200

    apply bottom stack patch

--- config.h
+++ config.h
@@ -36,11 +36,14 @@ static const float mfact      = 0.55; /* factor of master area size [0.05..0.95]
 static const int nmaster      = 1;    /* number of clients in master area */
 static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
 
+#include "bstack.c"
+#include "bstackhoriz.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-//	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "TTT",      bstack },
+	{ "===",      bstackhoriz },
 };
 
 /* key definitions */
@@ -74,9 +77,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_n,      zoom,           {0} },
 	{ MODKEY,                       XK_y,      view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_z,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
-	//{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_z,      setlayout,      {.v = &layouts[0]} }, // tile
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} }, // monocle
+	{ MODKEY,                       XK_j,      setlayout,      {.v = &layouts[2]} }, // bstack
+	{ MODKEY,                       XK_w,      setlayout,      {.v = &layouts[3]} }, // hstack
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
